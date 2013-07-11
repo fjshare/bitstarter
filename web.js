56d6015abe452d01170b62d9b7e0fe566e974d45
Fabricio Santos <fjshare@gmail.com>
2013-07-11 22:44:26 +0000

    Changed Buffer and Sync File

--- web.js
+++ web.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var file = ('index.html');
 
 var app = express.createServer(express.logger());
 
-var mesg = function (err, file) {
-
-var buffer = new Buffer(fs.readFileSync(file));
-return(buffer.toString('utf-8'));
-
-
-)};
-
 
 app.get('/', function(request, response) {
-  response.send(mesg());
-
+  
+    var buffer = new Buffer(fs.readFileSync('./index.html'));
+    response.send(buffer.toString('utf8'));
 });
 
 
