2ca1c9e8320ebafa8b3e893fd829e4796d9a0c92
Fabricio Santos <fjshare@gmail.com>
2013-07-11 19:45:34 +0000

    Use fs.readFileSync to read a remote html file

--- web.js
+++ web.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var fs = require('fs');
+var file = ('index.html');
 
 var app = express.createServer(express.logger());
 
+var mesg = function (err, file) {
+
+var buffer = new Buffer(fs.readFileSync(file));
+return(buffer.toString('utf-8'));
+
+
+)};
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(mesg());
+
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
